c918ed6ebe8ab976c50e10715e9896c5128b2bee
Nursultan1125 <zholnurs@gmail.com>
2017-04-19 18:01:12 +0600

    add scprits

--- script2.py
+++ script2.py
@@ -1,4 +1,5 @@
-print("Общество в начале XXI века\n\n")
+# -- coding: utf-8 --
+from mypack import modul_scr2
 
 age = 0
 
@@ -6,23 +7,8 @@ while True:
     try:
         age = int(input("Введите ваш возраст: "))
         break
-    except(ValueError):
+    except ValueError:
         print("Пожалуйста введите коректный возраст !!!")
 
-if(age >= 0) and (age < 7):
-    print("Вам в детский сад")
-
-elif(age >= 7) and (age < 18):
-    print("Вам школу")
-
-elif(age >= 18) and (age < 25):
-    print("Вам в профессиональное учебное заведение")
-
-elif(age >= 25) and (age < 60):
-    print("Вам на работу")
-
-elif(age >= 60) and (age <= 120):
-    print("Вам предоставляется выбор")
-else:
-    a = "Ошибка! Это программа для людей!\n"
-    print(a*5)
+print(age)
+print(modul_scr2.func(age))
